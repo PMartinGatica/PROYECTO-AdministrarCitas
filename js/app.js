657039f3f6f6cdc54122627ae39457e86386c5da
Pablo Gatica <ga77ica@hotmail.com>
2023-05-25 21:01:45 -0300

    Form-netlify2

--- js/app.js
+++ js/app.js
@@ -13,6 +13,13 @@ class Citas{
     constructor(){
         this.citas = []
     }
+    agregarCita(cita){
+        this.citas = [...this.citas,cita]
+        console.log (this.citas);
+    }
+    eliminarCita(id){
+        this.citas = this.citas.filter (cita => cita.id !==id)
+    }
 }
 
 class UI{
@@ -42,6 +49,68 @@ class UI{
         setTimeout(() => {
             divMensaje.remove();
         }, 5000);
+    }
+
+    imprimirCitas({citas}){
+
+        this.limpiarHTML();
+        citas.forEach(cita => {
+            const{mascota,propietario,telefono,fecha,hora,sintomas,id}=cita
+            
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita','p-3');
+            divCita.dataset.id = id;
+
+            //Scripting de los elementos de la cita
+
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title','font-weight-bolder');
+            mascotaParrafo.textContent = mascota;
+            
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML =`
+                <span class = "font-wight-bolder">Propietario: </span> ${propietario} `;
+
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML =`
+            <span class = "font-wight-bolder">Telefono: </span> ${telefono} `;
+
+            const fechaParrafo = document.createElement('p');
+            fechaParrafo.innerHTML =`
+            <span class = "font-wight-bolder">Fecha: </span> ${fecha} `;
+
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML =`
+            <span class = "font-wight-bolder">hora: </span> ${hora} `;
+            
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML =`
+            <span class = "font-wight-bolder">Sintomas: </span> ${sintomas} `;
+
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn','btn-danger','mr-2');
+            btnEliminar.innerHTML ='Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/></svg>';
+
+            btnEliminar.onclick = ()=> eliminarCita(id);
+
+            //Agregar los parrafos al divCita
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
+            //agregar las citas al HTML
+            contenedorCitas.appendChild(divCita);
+        });
+    };
+
+    limpiarHTML(){
+        while(contenedorCitas.firstChild){
+            contenedorCitas.removeChild(contenedorCitas.firstChild)
+        }
     }
 }
 
@@ -86,6 +155,34 @@ function nuevaCita(e){
     ui.imprimirAlerta('Todos los campos son obligatorios','error');
     return;
 }
- 
- 
+//Generar ID Unico
+
+citaObj.id = Date.now();
+
+//Crear una nueva cita
+administrarCitas.agregarCita({...citaObj});
+//Reiniciar objeto para la validacion
+reiniciarObjeto();
+//Reiniciar Formulario
+formulario.reset();
+
+//Mostrar el HTML
+ui.imprimirCitas(administrarCitas)
 }
+function reiniciarObjeto(){
+    citaObj.mascota='',
+    citaObj.propietario='',
+    citaObj.telefono='',
+    citaObj.fecha='',
+    citaObj.hora='',
+    citaObj.sintomas=''
+ }
+ function eliminarCita(id){
+    //Elminar Cita
+    administrarCitas.eliminarCita(id)
+    //Mostrar Msj
+    ui.imprimirAlerta('La cita se elimino correctamente')
+
+    //Refresacar HTML
+    ui.imprimirCitas(administrarCitas);
+ }
